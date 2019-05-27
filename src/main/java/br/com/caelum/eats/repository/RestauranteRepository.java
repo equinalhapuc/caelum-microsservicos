29ef171727e8e4b4d7ce16b050f2906fdf8b0345
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-05-27 19:05:26 -0300

    @Transactional no Controller

--- src/main/java/br/com/caelum/eats/repository/RestauranteRepository.java
+++ src/main/java/br/com/caelum/eats/repository/RestauranteRepository.java
@@ -2,8 +2,6 @@ package br.com.caelum.eats.repository;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -25,7 +23,6 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
 	Restaurante findByUser(User user);
 
-	@Transactional
 	@Modifying(clearAutomatically = true)
 	@Query("update Restaurante r set r.aprovado = true where r.id = :id")
 	void aprovaPorId(@Param("id") Long id);
